52a6c86a23228d939e46841a1c7a560ac26c46a6
jinnkeyxx <jinnkeyxx@gmail.com>
2020-12-20 16:05:37 +0700

    commit limit carauseo

--- src/Rokida/Components/CarauseoProduct.jsx
+++ src/Rokida/Components/CarauseoProduct.jsx
@@ -27,17 +27,26 @@ const CarauseoProduct = () => {
             {   
               breakpoint: 1024,
               settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
+                slidesToShow: 5,
+                slidesToScroll: 1,
                 infinite: true,
                 
               }
             },
+            {   
+                breakpoint: 991,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 1,
+                  infinite: true,
+                  
+                }
+              },
             {
               breakpoint: 600,
               settings: {
                 slidesToShow: 2,
-                slidesToScroll: 2,
+                slidesToScroll: 1,
                 initialSlide: 2
               }
             },
