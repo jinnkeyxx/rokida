61800a82f46b903bce1826eb357041cb5229b6c6
lythagiang83 <lythagiang@gmail.com>
2020-12-25 14:49:42 +0700

    giangly

--- src/Rokida/Components/Suggestion.jsx
+++ src/Rokida/Components/Suggestion.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import Slider from "react-slick";
 import poster_Rokida from '../images/poster-rokida-onl.png'
+import ProductItem from './ProductItem'
 
 
 
@@ -11,13 +12,14 @@ const Suggestion = () => {
             <Container className="mt-5">
                 <h3 style={{ fontWeight: 'bold', textTransform: 'uppercase' }}>Gợi ý hôm nay</h3>
                 <Row className="my-3">
-                    <Col className="text-center">
-                        <img src={poster_Rokida} alt="" />
+                    <Col md={12} lg={9} sm={12}>
+                        <ProductItem />
                     </Col>
-                    <Col>
-                    
+                    <Col className=" text-center text-lg-right" >
+                        <img src={poster_Rokida} alt="" className="img-fluid" />
                     </Col>
-                    
+
+
                 </Row>
             </Container>
         </>
--- src/Rokida/Components/SuperSale.jsx
+++ src/Rokida/Components/SuperSale.jsx
@@ -1,12 +1,6 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import banner_SuperSale1 from '../images/banner-sieusale-1.png'
-import banner_SuperSale2 from '../images/bannerSale.png'
-import banner_SuperSale3 from '../images/banner-sieusale-2.png'
-import banner_SuperSale4 from '../images/banner-sieusale-3.png'
-import product_SuperSale1 from '../images/product-sale-2.png'
-import product_SuperSale2 from '../images/product-sale-3.png'
-import product_SuperSale3 from '../images/product-sale-4.png'
 import ProductItem from './ProductItem'
 
 const SuperSale = () => {
